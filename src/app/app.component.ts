1294395fca855d551573b3418d35cf7501fe7dfd
Brad <brmccr@gmail.com>
2019-11-13 15:31:49 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+// import { HttpClientModule} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
+// import { Post } from './post';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+
+// import 'rxjs/add/operator/catch';
+// import 'rxjs/add/operator/retry';
+// import 'rxjs/add/observable/of';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'swapi-project';
+  people: string = "";
+  responsetwo: any;
+  // posts : any;
+
+  // readonly ROOT_URL = 'https://jsonplaceholder.typicode.com/'
+  constructor(private http: HttpClient ) {}
+
+  ngOnInit() {
+    let obs = this.http.get('https://swapi.co/api/people/1')
+    obs.subscribe((response) => console.log(response));
+  }
+  
+  search() {
+    this.http.get('https://swapi.co/api/' + this.people)
+    .subscribe((responsetwo) => {
+      this.responsetwo = responsetwo;
+      console.log(this.responsetwo)
+    })
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,24 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// import { HttpClient } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    HttpClientModule,
+    FormsModule
+    // HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
